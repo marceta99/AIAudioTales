fc43f83eaf3598d8e519bb455682120f0ff1dc64
Mihailo Marcetic <marcetaking@gmail.com>
2024-08-30 00:50:19 +0200

    Feature: Add tuneling to https for backend and frontend using ngrok and mstunels

--- AIAudioTalesClient/src/environment/environment.ts
+++ AIAudioTalesClient/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:5000/api/",
+  apiUrl: "https://34b910nm-5000.euw.devtunnels.ms/api/",
   clientId: "109854671037-dsb45t436gcgtfqvjdshf756o7ith0ma.apps.googleusercontent.com",
   stripePublicKey: "pk_test_51Og1xQBQEYQBClyqqZDorWZzzYSncrRIj7NOQWEuseY10iYNLzAK4FgUykXs3wVatbNlwAs5IdX6j2OiDdxRlqQC001PKgdOLE"
 };
--- AIAudioTalesServer/AIAudioTalesServer/Program.cs
+++ AIAudioTalesServer/AIAudioTalesServer/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Stripe;
 using System.Text;
@@ -64,6 +65,7 @@ builder.Services.AddAuthentication(x =>
 .AddCookie(x =>
 {
     x.Cookie.Name = "token";
+
 })
 .AddJwtBearer(x =>
 {
@@ -95,10 +97,13 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "CorsPolicy", builder =>
     {
-        builder.WithOrigins(configuration["ApplicationSettings:ClientUrl"])
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+        builder
+          .SetIsOriginAllowedToAllowWildcardSubdomains()
+          .WithOrigins("https://*.ngrok-free.app") // Allow all ngrok origins
+          .AllowAnyMethod()
+          .AllowAnyHeader()
+          .AllowCredentials();
+
     });
 });
 
@@ -118,9 +123,30 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseCors("CorsPolicy"); // Move this line up
 
-app.UseCors("CorsPolicy");
+app.Use(async (context, next) =>
+{
+    var origin = context.Request.Headers["Origin"].ToString();
+    if (!string.IsNullOrEmpty(origin))
+    {
+        context.Response.Headers.Add("Access-Control-Allow-Origin", origin);
+    }
+    context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    // Handle preflight requests
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.CompleteAsync();
+        return;
+    }
+
+    await next();
+});
+
 app.UseHttpsRedirection();
 
 // Serve files from "uploads" directory
@@ -133,6 +159,7 @@ app.UseStaticFiles(new StaticFileOptions
 
 app.UseAuthentication();
 app.UseMiddleware<UserContextMiddleware>();
+app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
