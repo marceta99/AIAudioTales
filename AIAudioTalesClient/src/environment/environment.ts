be3f8e7171724a02b90a5bea462ce7802515c226
Mihailo Marcetic <marcetaking@gmail.com>
2024-08-31 00:52:47 +0200

    Feature: Set public avaialbe link for book audio parts

--- AIAudioTalesClient/src/environment/environment.ts
+++ AIAudioTalesClient/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "https://34b910nm-5000.euw.devtunnels.ms/api/",
+  apiUrl: "https://34b910nm-44320.euw.devtunnels.ms/api/",
   clientId: "109854671037-dsb45t436gcgtfqvjdshf756o7ith0ma.apps.googleusercontent.com",
   stripePublicKey: "pk_test_51Og1xQBQEYQBClyqqZDorWZzzYSncrRIj7NOQWEuseY10iYNLzAK4FgUykXs3wVatbNlwAs5IdX6j2OiDdxRlqQC001PKgdOLE"
 };
