1fe82cdeb0136cfd95dd8f2e93eb650b4ab0c7f9
Mihailo Marcetic <marcetaking@gmail.com>
2024-11-16 17:46:50 +0100

    Feature: Add previous search list on search component

--- AIAudioTalesClient/src/app/home/search/search.component.ts
+++ AIAudioTalesClient/src/app/home/search/search.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
-
+  previousSearches: string[] = [
+    'Linkin Park',
+    'Lil Yachty',
+    'Lady Gaga',
+    'Legends Never Die',
+    'Drake',
+    'Post Malone',
+    'Linkin Park',
+    'Lil Yachty',
+    'Lady Gaga',
+    'Legends Never Die',
+    'Drake',
+    'Post Malone',
+    'Legends Never Die',
+    'Drake',
+    'Post Malone',
+    'Linkin Park',
+    'Lil Yachty',
+    'Lady Gaga',
+    'Legends Never Die',
+    'Drake',
+    'Post Malone',
+  ];
 }
