5c054562379769172d4b9a1fc685e555290088ca
Mihailo Marcetic <marcetaking@gmail.com>
2024-11-16 18:49:54 +0100

    Feature: Add initial text when there is no previous searches

--- AIAudioTalesClient/src/app/home/search/search.component.ts
+++ AIAudioTalesClient/src/app/home/search/search.component.ts
@@ -1,32 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
-  previousSearches: string[] = [
-    'Linkin Park',
-    'Lil Yachty',
-    'Lady Gaga',
-    'Legends Never Die',
-    'Drake',
-    'Post Malone',
-    'Linkin Park',
-    'Lil Yachty',
-    'Lady Gaga',
-    'Legends Never Die',
-    'Drake',
-    'Post Malone',
-    'Legends Never Die',
-    'Drake',
-    'Post Malone',
-    'Linkin Park',
-    'Lil Yachty',
-    'Lady Gaga',
-    'Legends Never Die',
-    'Drake',
-    'Post Malone',
-  ];
+export class SearchComponent implements OnInit {
+  searchHistory: string[] = [];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.fetchSearchHistory();
+  }
+
+  private fetchSearchHistory(): void{
+    this.bookService.getSearchHistory().subscribe({
+      next: (searchHistory : string[] ) => {
+        console.log("searchHistory",searchHistory);
+        this.searchHistory = searchHistory;
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    })
+  }
 }
