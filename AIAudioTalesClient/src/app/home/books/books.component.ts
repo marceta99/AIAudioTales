683ced1efc7f10f068730c793a038b5f1afb293b
Mihailo Marcetic <marcetaking@gmail.com>
2024-12-04 00:05:32 +0100

    Feature: Add banner slider on home screen

--- AIAudioTalesClient/src/app/home/books/books.component.ts
+++ AIAudioTalesClient/src/app/home/books/books.component.ts
@@ -12,6 +12,14 @@ export class BooksComponent implements OnInit {
   homePageCategories : number[] = [5, 6];
   categories: { books: ReturnBook[], category: BookCategory }[] = [];
 
+  ads = [
+    { image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXICWl6tskBwvFowpPtfNagBJCQSZVj4hDtA&s', alt: 'Banner 1' },
+    { image: 'https://www.catholicmom.com/hubfs/20240104%20CRangel%201.png', alt: 'Banner 2' },
+    { image: 'https://www.thebookwarehouse.com.au/wp-content/uploads/2021/09/image-30.png', alt: 'Banner 3' }, // Add more ads as needed
+  ];
+  
+  activeAdIndex = 0;
+
   constructor(private bookService: BookService, private spinnerService: LoadingSpinnerService) {}
 
   ngOnInit(): void {
@@ -37,5 +45,16 @@ export class BooksComponent implements OnInit {
         this.spinnerService.setLoading(false);
     }
     })
+  }
+
+  onScroll(slider: HTMLElement): void {
+    const scrollLeft = slider.scrollLeft;
+    const width = slider.offsetWidth;
+    this.activeAdIndex = Math.round(scrollLeft / width);
+  }
+  
+  scrollToAd(index: number, slider: HTMLElement): void {
+    const width = slider.offsetWidth;
+    slider.scrollTo({ left: index * width, behavior: 'smooth' });
   }
 }
