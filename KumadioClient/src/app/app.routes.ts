153f17d72f499e4bfb4df268e6b196672af5588f
Mihailo Marcetic <marcetaking@gmail.com>
2025-02-18 00:59:51 +0100

    Feature: Add kumadio client to android studio

--- KumadioClient/src/app/app.routes.ts
+++ KumadioClient/src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
--- KumadioClient/src/environments/environment.prod.ts
+++ KumadioClient/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://34b910nm-44320.euw.devtunnels.ms/api/",
+  clientId: "109854671037-dsb45t436gcgtfqvjdshf756o7ith0ma.apps.googleusercontent.com",
 };
